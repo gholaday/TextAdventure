fdaf01eaf4c9e81e96718a391484ea9191d3c675
Garrett Holaday <gholaday@vols.utk.edu>
2017-09-19 18:24:51 -0500

    Redid map generation to use sprites instead of line renderers

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -7,21 +7,20 @@ public class Map : MonoBehaviour {
 
 	public float spacing = 1.0f;
 	public GameObject roomText;
+	public GameObject mapLine;
 	public Camera cam;
 
 	public Dictionary<string, MapRoom> mapRooms = new Dictionary<string, MapRoom>();
 
-	string[] map = {"Entrance", "north", "Office", "south", "west", "Atrium", "north", "north", "Basement"};
+	string[] map = {"Entrance", "north", "Office", "south", "west", "Atrium", "north", "north", "Basement", "west", "west", "south", "Bedroom"};
+
 	Vector2 currentPos = Vector2.zero;
-	LineRenderer lr;
-	int lrPos = 1;
 	Vector3 camMoveDir;
 	Vector3 lastPos;
+	GameObject mapParent;
 
 	void Awake()
 	{
-		lr = GetComponent<LineRenderer> ();
-
 		InitialSetup ();
 		GenerateMap ();
 	}
@@ -30,39 +29,58 @@ public class Map : MonoBehaviour {
 	{
 		camMoveDir = cam.transform.position;
 		currentPos = transform.position;
-
-		lr.positionCount = map.Length + 1;
-		lr.SetPosition (0, currentPos);
 	}
 
 	void GenerateMap()
 	{
+		mapParent = new GameObject ("Map Sprites");
+
+		GameObject line = null;
+		Vector3 oldPos = Vector3.zero;
+
 		foreach(string word in map)
 		{
+			line = InstantiateMapLine ();
+			oldPos = currentPos;
+
 			switch (word)
 			{
 			case "north":
 				currentPos += Vector2.up * spacing;
+				if (line != null) DrawLine (line, oldPos, currentPos);
 				break;
 			case "south":
 				currentPos += Vector2.down * spacing;
+				if (line != null) DrawLine (line, oldPos, currentPos);
 				break;
 			case "east":
 				currentPos += Vector2.right * spacing;
+				if (line != null) DrawLine (line, oldPos, currentPos);
 				break;
 			case "west":
 				currentPos += Vector2.left * spacing;
+				if (line != null) DrawLine (line, oldPos, currentPos);
 				break;
 			default:
 				DrawRoom (word);
 				break;
 
 			}
-
-			lr.SetPosition (lrPos, currentPos);
-			lrPos++;
 		}
+	}
 
+	void DrawLine(GameObject line, Vector3 start, Vector3 end)
+	{
+		Vector3 center = (start + end) / 2f;
+		line.transform.position = center;
+
+		Vector3 dir = end - start;
+		dir = Vector3.Normalize (dir);
+		line.transform.right = dir;
+
+		Vector3 scale = new Vector3 (1, 1, 1);
+		scale.x = Vector3.Distance (start, end) + 1f;
+		line.transform.localScale = scale;
 	}
 
 	void AddRoomToDict(MapRoom room)
@@ -74,9 +92,19 @@ public class Map : MonoBehaviour {
 
 	}
 
+	GameObject InstantiateMapLine()
+	{
+		GameObject go = Instantiate (mapLine, currentPos, Quaternion.identity);
+		go.transform.Translate (0, 0, -10);
+		go.transform.SetParent (mapParent.transform);
+
+		return go;
+	}
+
 	void DrawRoom(string name)
 	{
 		GameObject go = Instantiate (roomText, currentPos, Quaternion.identity);
+		go.transform.SetParent (mapParent.transform);
 		TextMeshPro tmp = go.GetComponent<TextMeshPro>();
 		MapRoom room = go.GetComponent<MapRoom>();
 		room.roomName = name;
@@ -91,7 +119,6 @@ public class Map : MonoBehaviour {
 		if(cam.transform.position != camMoveDir)
 		{
 			cam.transform.position = Vector3.Lerp (cam.transform.position, camMoveDir, Time.deltaTime * 5);
-
 		}
 	}
 
